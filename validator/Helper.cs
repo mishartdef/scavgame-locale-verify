7842e556e5cab46df176667829f863caf5f33ed7
mishartdef <misha790@bk.ru>
2025-12-21 01:13:45 +0500

    Add localization validator and GitHub action

---
+++ validator/Helper.cs
@@ -0,0 +1,69 @@
+using System.Text.Json;
+
+namespace validator;
+
+public static class Helper
+{
+    private static bool _hasCriticalError = false;
+    
+    /// <summary>
+    /// Returns a list of all keys from the locale file.
+    /// </summary>
+    /// <param name="path">Path to the locale file.</param>
+    /// <returns>A list of all keys in the given JSON file.</returns>
+    public static List<string> GetFullKeys(string path)
+    {
+        try 
+        {
+            var keys = new List<string>();
+            using var doc = JsonDocument.Parse(File.ReadAllText(path));
+            ExtractKeys(doc.RootElement, "", keys);
+            return keys;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"::error file={Path.GetFileName(path)}::Failed to parse JSON: {ex.Message}");
+            _hasCriticalError = true;
+            return new List<string>();
+        }
+    }
+
+    /// <summary>
+    /// Recursively extracts all keys from the JSON element.
+    /// </summary>
+    /// <param name="element">The JSON element to extract keys from.</param>
+    /// <param name="prefix">The current path.</param>
+    /// <param name="keys">The list of keys to add to.</param>
+    public static void ExtractKeys(JsonElement element, string prefix, List<string> keys)
+    {
+        if (element.ValueKind == JsonValueKind.Object)
+        {
+            foreach (var property in element.EnumerateObject())
+            {
+                string currentPath = string.IsNullOrEmpty(prefix) ? property.Name : $"{prefix}.{property.Name}";
+                keys.Add(currentPath);
+                ExtractKeys(property.Value, currentPath, keys);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Marks the validation as failed.
+    /// </summary>
+    public static void MarkAsFailed() => _hasCriticalError = true;
+
+    /// <summary>
+    /// Exit with error code, if there is a critical error.
+    /// </summary>
+    /// <param name="hasCriticalError"></param>
+    public static void ValidationCheck()
+    {
+        if (_hasCriticalError)
+        {
+            Console.WriteLine("\n[RESULT] Validation failed. Fix the errors above.");
+            Environment.Exit(1);
+        }
+        
+        Console.WriteLine("\n[RESULT] All checks passed successfully.");
+    }
+}
---
+++ validator/Program.cs
@@ -0,0 +1,73 @@
+﻿using static validator.Helper;
+
+// Getting path to locales directory
+string localeDir = Directory.GetCurrentDirectory();
+
+// Getting path to original locale file
+string originalFilePath = Path.Combine(localeDir, "EN.json");
+
+if (!File.Exists(originalFilePath))
+{
+    Console.WriteLine($"::error::Original locale file not found: {originalFilePath}");
+    MarkAsFailed();
+}
+else
+{
+    // Getting all keys from original locale file
+    var originalKeys = GetFullKeys(originalFilePath);
+
+    // Getting all verify files from locale directory
+    var filesToVerify = args.Length > 0 ? args 
+        : Directory.GetFiles(localeDir, "*.json");
+
+    // Checking all locale files
+    foreach (var filePath in filesToVerify)
+    {
+        string fileName = Path.GetFileName(filePath);
+        try
+        {
+            Console.WriteLine($"\n>>> Checking: {fileName}");
+            var currentKeys = GetFullKeys(filePath);
+
+            var missingKeys = originalKeys.Except(currentKeys).ToList();
+            var extraKeys = currentKeys.Except(originalKeys).ToList();
+
+            if (!missingKeys.Any() && !extraKeys.Any())
+            {
+                Console.WriteLine($"[Pass] {fileName} is valid.");
+            }
+            else
+            {
+                if (missingKeys.Any())
+                {
+                    // Missing keys are critical, without them game will use keys name (e.g. bandage bandagedcs)
+                    Console.WriteLine($"::error file={fileName}::Missing {missingKeys.Count} keys.");
+                    Console.WriteLine($"[Error] Missing keys: ({missingKeys.Count}):");
+                    missingKeys.ForEach(k => Console.WriteLine($"   - {k}"));
+                    MarkAsFailed();
+                }
+
+                if (extraKeys.Any())
+                {
+                    // Extra keys are not that critical, but they can be confusing for the translators
+                    Console.WriteLine($"::warning file={fileName}::Found {extraKeys.Count} extra keys.");
+                    Console.WriteLine($"[Warning] Extra keys: ({extraKeys.Count}):");
+                    extraKeys.ForEach(k => Console.WriteLine($"   + {k}"));
+                }
+            }
+        }
+        // If there is a critical error in parse (e.g. corrupted json), then exit with error code
+        catch (Exception ex)
+        {
+            Console.WriteLine($"::error file={fileName}::Critical error: {ex.Message}");
+            MarkAsFailed();
+        }
+    }
+}
+
+
+
+// If there was a critical error (missing keys or exception), then exit with error code
+ValidationCheck();
+
+Console.WriteLine("\n[RESULT] All files passed successfully!");
